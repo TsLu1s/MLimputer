331bb060d0b039559324c34e59c8fef724b01ee6
TsLuis <luisfssantos98@hotmail.com>
2023-02-07 11:14:25 +0000

    mlimputer update

---
+++ setup.py
@@ -0,0 +1,47 @@
+import setuptools
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="MLimputer",
+    version="0.0.95",
+    description="MLimputer - Null Imputation Framework for Supervised Machine Learning",
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    url="https://github.com/TsLu1s/MLimputer",
+    author="Luís Santos",
+    author_email="luisf_ssantos@hotmail.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # Indicate who your project is intended for
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Customer Service",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Telecommunications Industry",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    py_modules=["mlimputer"],
+    package_dir={"": "src/mlimputer"},  
+    keywords=[
+        "data science",
+        "machine learning",
+        "data preprecessing",
+        "null imputation",
+        "predictive null imputation",
+        "multiple null imputation",
+        "automated machine learning",
+    ],           
+    install_requires=open("requirements.txt").readlines(),
+)
