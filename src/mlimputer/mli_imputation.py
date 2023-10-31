456c044ae471bc48c42b7db32f7c43cc5bd18ff0
Luís Santos <80392073+TsLu1s@users.noreply.github.com>
2023-10-31 00:45:17 +0000

    Update mli_imputation.py

--- src/mlimputer/mli_imputation.py
+++ src/mlimputer/mli_imputation.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import numpy as np
-import sys
 from tqdm import tqdm
-import atlantic as atl
+from atlantic.processing import AutoLabelEncoder
+from atlantic.imputation import AutoSimpleImputer
 from .mli_parameters import imputer_parameters
 from .mli_model_selection import missing_report, imput_models
 
@@ -44,18 +43,20 @@ def fit_imput(dataset:pd.DataFrame,
         
         train=df.iloc[train_index]
         
-        # Fit the label encoding method in categorical columns
-        le_fit=atl.fit_Label_Encoding(train,target)
-        train=atl.transform_Label_Encoding(train,le_fit)
+        cat_cols=[col for col in train.select_dtypes(include=['object']).columns if col != target]
+        
+        ## Create Label Encoder
+        encoder = AutoLabelEncoder()
+        ## Fit Label Encoder
+        encoder.fit(train[cat_cols])
+        # Transform the DataFrame using Label
+        train=encoder.transform(X=train)
         
         # Fit the simple imputation method in input columns
-        imputer_simple=atl.fit_SimpleImp(dataset=train,
-                                         target=target,
-                                         strat='mean')
+        simple_imputer = AutoSimpleImputer(strategy='mean')
+        simple_imputer.fit(train)  # Fit on the Train DataFrame
+        train = simple_imputer.transform(train.copy())  # Transform the Train DataFrame
         
-        train=atl.transform_SimpleImp(dataset=train,
-                                      target=target,
-                                      imputer=imputer_simple)
         # Fit the imputation model
         model = imput_models(train=train,
                              target=target,
@@ -66,13 +67,13 @@ def fit_imput(dataset:pd.DataFrame,
         if c==0:
             imp_config = {target:{'model_name':imput_model,
                                   'model':model,
-                                  'pre_process':le_fit,
-                                  'input_nulls':imputer_simple}}
+                                  'pre_process':encoder,
+                                  'imputer':simple_imputer}}
         elif c>0:
             imp_config_2 = {target:{'model_name':imput_model,
                                     'model':model,
-                                    'pre_process':le_fit,
-                                    'input_nulls':imputer_simple}}
+                                    'pre_process':encoder,
+                                    'imputer':simple_imputer}}
             imp_config.update(imp_config_2)
         c+=1
         
@@ -104,14 +105,13 @@ def transform_imput(dataset:pd.DataFrame,
         test_index = df_[df_[target].isnull()].index.tolist()
         test_df=df_.iloc[test_index]
         
-        le_fit=fit_configs[target]['pre_process']
-        test_df=atl.transform_Label_Encoding(test_df,le_fit)
-        input_num_cols = atl.num_cols(test_df, target)
+        encoder=fit_configs[target]['pre_process']
+        # Transform the DataFrame using Label
+        test_df=encoder.transform(X=test_df)
         
-        imputer_simple=fit_configs[target]['input_nulls']
-        test_df=atl.transform_SimpleImp(dataset=test_df,
-                                        target=target,
-                                        imputer=imputer_simple)
+        # Impute the DataFrame using Simple Imputer
+        simple_imputer=fit_configs[target]['imputer']
+        test_df = simple_imputer.transform(test_df.copy())  
         
         sel_cols=list(test_df.columns)
         sel_cols.remove(target)
